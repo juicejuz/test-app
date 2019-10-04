a312819aa79d13e739d16e248194827bb1fd8e94
juicejuz <dgeiss438@gmail.com>
2019-10-04 07:49:34 +0200

    add some tests

---
+++ api/tests/unit/middleware/auth.spec.js
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2019 OpenAdvice & Vodafone SM-4.0 TV
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * --------------------------------------------------------------------------------
+ * Description:
+ * --------------------------------------------------------------------------------
+ */
+
+const { User } = require('../../../models/user');
+const auth = require('../../../middleware/auth');
+const mongoose = require('mongoose');
+
+describe('auth middleware', () => {
+  it('should populate req.user with the payload of a valid JWT', () => {
+    const user = {
+      _id: mongoose.Types.ObjectId().toHexString(),
+      isAdmin: true
+    };
+    const token = new User(user).generateAuthToken();
+    const req = {
+      header: jest.fn().mockReturnValue(token)
+    };
+    const res = {};
+    const next = jest.fn();
+
+    auth(req, res, next);
+
+    expect(req.user).toMatchObject(user);
+  });
+});
---
+++ api/tests/unit/models/user.spec.js
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2019 OpenAdvice & Vodafone SM-4.0 TV
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * --------------------------------------------------------------------------------
+ * Description:
+ * --------------------------------------------------------------------------------
+ */
+
+const { User } = require('../../../models/user');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const mongoose = require('mongoose');
+
+describe('user.generateAuthToken', () => {
+  it('should return a valid JWT', () => {
+    const payload = {
+      _id: new mongoose.Types.ObjectId().toHexString(),
+      isAdmin: true
+    };
+    const user = new User(payload);
+    const token = user.generateAuthToken();
+    const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+    expect(decoded).toMatchObject(payload);
+  });
+});
