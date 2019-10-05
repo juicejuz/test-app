ef306d6f0074417c9f6f06191764b25cf0b896a1
juicejuz <dgeiss438@gmail.com>
2019-10-05 04:36:55 +0200

    change process.env in config

--- api/startup/config.js
+++ api/startup/config.js
@@ -23,7 +23,7 @@
 const config = require('config');
 
 module.exports = function() {
-  if (!process.env.JWT_PRIVATE_KEY || !config.get('jwtPrivateKey')) {
+  if (!config.get('jwtPrivateKey')) {
     throw new Error('FATAL ERROR: jwtPrivateKey is not defined.');
   }
 };
